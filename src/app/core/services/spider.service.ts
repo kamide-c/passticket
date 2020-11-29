efb556e80ad4205b277facf8a12f97468f91b2bc
Cesar Martino <cesar.martino@zflow.com.br>
2020-11-29 11:16:59 -0300

    proxy config

--- src/app/core/services/spider.service.ts
+++ src/app/core/services/spider.service.ts
@@ -1,23 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpiderService {
-  environmentUrl = 'Debug api';
-
-  constructor(private _httpClient: HttpClient) {
-    this.environmentUrl = environment.baseUrl;
-  }
+  constructor(private _httpClient: HttpClient) {}
 
   public getEvents(id?: string): Observable<any> {
     const path = '/' + id;
 
-    return this._httpClient.get(
-      this.environmentUrl + '/Events' + (id ? path : '')
-    );
+    return this._httpClient.get('/api/Events' + (id ? path : ''));
   }
 }
