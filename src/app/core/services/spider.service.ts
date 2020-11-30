78ebdc289be84d61d43947a60d3e4dae6919ecc6
Cesar Martino <cesar.martino@zflow.com.br>
2020-11-29 21:47:32 -0300

    add httpheaders

--- src/app/core/services/spider.service.ts
+++ src/app/core/services/spider.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,8 +9,15 @@ export class SpiderService {
   constructor(private _httpClient: HttpClient) {}
 
   public getEvents(id?: string): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        responseType: 'text',
+      }),
+    };
+
     const path = '/' + id;
 
-    return this._httpClient.get('/api/Events' + (id ? path : ''));
+    return this._httpClient.get('/api/Events' + (id ? path : ''), httpOptions);
   }
 }
