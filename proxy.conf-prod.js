fd75a41abfba778cdb5f0f58c8e47dbbe8b15b48
Cesar Martino <cesar.martino@zflow.com.br>
2021-01-10 17:36:13 -0300

    configure prod proxy

--- proxy.conf-prod.js
+++ proxy.conf-prod.js
@@ -1,9 +1,10 @@
 const PROXY_CONFIG = [
   {
-    context: [''],
+    context: ['/api'],
     target: 'http://40.74.238.182:8080/',
     secure: false,
     logLevel: 'debug',
+    pathRewrite: { '^/api': '' },
     changeOrigin: true
   }
 ];
