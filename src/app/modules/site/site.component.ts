c605fb8f086e9dc3f9d398be15746855d5d31452
Cesar Martino <cesar.martino@zflow.com.br>
2021-02-20 01:27:38 -0300

    scroll to top button

--- src/app/modules/site/site.component.ts
+++ src/app/modules/site/site.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { PerfectScrollbarComponent } from 'ngx-perfect-scrollbar';
 import { ScrollPaginationService } from 'src/app/core/services/scroll-pagination/scroll-pagination.service';
 
 @Component({
@@ -7,6 +8,9 @@ import { ScrollPaginationService } from 'src/app/core/services/scroll-pagination
   styleUrls: ['./site.component.scss'],
 })
 export class SiteComponent implements OnInit {
+  @ViewChild(PerfectScrollbarComponent, { static: false })
+  componentRef?: PerfectScrollbarComponent;
+  detectScrollDown = false;
   constructor(
     private scrollPaginationService: ScrollPaginationService,
     private ref: ChangeDetectorRef
@@ -16,6 +20,20 @@ export class SiteComponent implements OnInit {
 
   getEvents() {
     this.scrollPaginationService.endPageSubject.next();
+    this.ref.detectChanges();
+  }
+
+  public scrollToTop(): void {
+    this.componentRef?.directiveRef?.scrollToTop();
+  }
+
+  public scrollDown(event: CustomEvent): void {
+    this.detectScrollDown = event.returnValue;
+    this.ref.detectChanges();
+  }
+
+  public psYReachStart(): void {
+    this.detectScrollDown = false;
     this.ref.detectChanges();
   }
 }
