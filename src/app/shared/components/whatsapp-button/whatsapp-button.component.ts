1d06ab16ca012b40176fc90b7ed943727c0a6145
Cesar Martino <cesar.martino@zflow.com.br>
2021-02-25 02:57:27 -0300

    feat:"whatsapp button

---
+++ src/app/shared/components/whatsapp-button/whatsapp-button.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-whatsapp-button',
+  templateUrl: './whatsapp-button.component.html',
+  styleUrls: ['./whatsapp-button.component.scss'],
+})
+export class WhatsappButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  goToWhatsapp(): void {
+    window.open(
+      'https://api.whatsapp.com/send?phone=seunumerodetelefone',
+      '_blank'
+    );
+  }
+}
