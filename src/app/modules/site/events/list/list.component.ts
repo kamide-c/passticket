6bea83338ebd7926afd7143bafa4c5cc6ce121ef
Cesar Martino <cesar.martino@zflow.com.br>
2021-03-22 17:37:15 -0300

    titulos

--- src/app/modules/site/events/list/list.component.ts
+++ src/app/modules/site/events/list/list.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {IEventFilter} from "../../../../core/interfaces/event";
-import {Subscription} from "rxjs";
-import {ScrollPaginationService} from "../../../../core/services/scroll-pagination/scroll-pagination.service";
-import {ActivatedRoute, Router} from "@angular/router";
+import { IEventFilter } from '../../../../core/interfaces/event';
+import { Subscription } from 'rxjs';
+import { ScrollPaginationService } from '../../../../core/services/scroll-pagination/scroll-pagination.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
   public filter: any = {
     page_number: 1,
-    page_size: 20
+    page_size: 20,
   };
   public filterChange: any;
+  public concatTerms!: string;
   constructor(
     private scrollPaginationService: ScrollPaginationService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title
   ) {
     let urlFilter = this.activatedRoute.snapshot.queryParams.filter;
     if (urlFilter) {
@@ -27,7 +31,19 @@ export class ListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    //
+    this.concatTerms =
+      (this.filter.search ? ' | ' + this.filter.search : '') +
+      (this.filter.city ? ' | ' + this.filter.city : '') +
+      (this.filter.date_begin
+        ? ' | ' + moment(this.filter.date_begin).format('d/MMM')
+        : '') +
+      (this.filter.date_end
+        ? ' | ' + moment(this.filter.date_end).format('d/MMM')
+        : '');
+
+    this.titleService.setTitle(
+      'PassTicket | Todos os eventos. Um só lugar.' + this.concatTerms
+    );
   }
 
   public doFilter(searchFilter: any): void {
